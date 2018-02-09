843a0bdaaad32af889da0199922212beda888ebe
Rubén Calvo Martínez <rubencalvoma@gmail.com>
2018-02-09 16:16:05 +0100

    - Minor changes.

--- src/main/java/com/planet_of_the_tapes/controller/mainController.java
+++ src/main/java/com/planet_of_the_tapes/controller/mainController.java
@@ -1,10 +1,20 @@
 package com.planet_of_the_tapes.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.planet_of_the_tapes.entity.User;
+import com.planet_of_the_tapes.repository.UserRepository;
 
 @Controller
 public class mainController {
+	
+	@Autowired
+	private UserRepository userRepository;
 	
 	@RequestMapping("/")
 	public String index() {
@@ -13,7 +23,12 @@ public class mainController {
 	}
 	
 	@RequestMapping("/admin")
-	public String admin() {
+	public String admin(Model model, HttpServletRequest request) {
+		User user = userRepository.findById(3);
+		User loggedAdmin = userRepository.findByName("Ruben");
+		model.addAttribute("admin", loggedAdmin);
+		model.addAttribute("user",user);
+		
 		
 		return "/admin/admin-dashboard";
 	}
---
+++ src/main/java/com/planet_of_the_tapes/controller/plistController.java
@@ -0,0 +1,26 @@
+package com.planet_of_the_tapes.controller;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.io.File;
+import java.time.LocalDateTime;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.planet_of_the_tapes.entity.User;
+import com.planet_of_the_tapes.repository.UserRepository;
+
+public class plistController {
+
+}
