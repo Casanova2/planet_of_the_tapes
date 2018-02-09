5ad38dd595431f27a756a6a1df50f2c1c89203bc
Rubén Calvo Martínez <rubencalvoma@gmail.com>
2018-02-09 14:21:33 +0100

    - Assets in static added correctly. - CSS and JS of the admin part. - View dashboard create. - Layouts of admin (head, header and footer(js)). - Rename the structure of folders in static/admin/assest because an error ass ocurred (conflict). - Fix the error incompatibility of css and js of index and admin parts.

--- src/main/java/com/planet_of_the_tapes/controller/mainController.java
+++ src/main/java/com/planet_of_the_tapes/controller/mainController.java
@@ -5,9 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class mainController {
+	
 	@RequestMapping("/")
 	public String index() {
 		
 		return "index";
+	}
+	
+	@RequestMapping("/admin")
+	public String admin() {
+		
+		return "/admin/admin-dashboard";
 	}
 }
