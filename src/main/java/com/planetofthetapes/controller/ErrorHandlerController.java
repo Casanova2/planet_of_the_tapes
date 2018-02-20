7b181ddc8fb6ae7ab73ad9246ad94425687795fe
AsierRuano <asierruano@gmail.com>
2018-02-20 10:57:54 +0100

    Fixed error controller

--- src/main/java/com/planetofthetapes/controller/ErrorHandlerController.java
+++ src/main/java/com/planetofthetapes/controller/ErrorHandlerController.java
@@ -2,18 +2,25 @@ package com.planetofthetapes.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @Controller
-public class ErrorController {
+public class ErrorHandlerController implements ErrorController{
 	
-	/*
+	
 	@RequestMapping("/error")
 	public String contact(Model model, HttpServletRequest request) {
 		return "error";
 	}
-*/
+
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return "/error";
+	}
+
 }
