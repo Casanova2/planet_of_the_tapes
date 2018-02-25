085119d7b25bc9aa3f4d7e4ca492644a2e37b2c7
Rubén Calvo Martínez <rubencalvoma@gmail.com>
2018-02-25 19:12:22 +0100

    - Upload image wip.

--- src/main/java/com/planetofthetapes/PlanetOfTheTapesApplication.java
+++ src/main/java/com/planetofthetapes/PlanetOfTheTapesApplication.java
@@ -1,18 +1,31 @@
 package com.planetofthetapes;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 
 import com.planetofthetapes.controller.MasterService;
+import com.planetofthetapes.controller.storage.StorageProperties;
+import com.planetofthetapes.controller.storage.StorageService;
 
 @SpringBootApplication
+@EnableConfigurationProperties(StorageProperties.class)
 public class PlanetOfTheTapesApplication extends MasterService{
 	
 	@Bean
 	public MasterService masterService(){
 		return new MasterService();
 	}
+	
+	@Bean
+    CommandLineRunner init(StorageService storageService) {
+        return (args) -> {
+            storageService.deleteAll();
+            storageService.init();
+        };
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlanetOfTheTapesApplication.class, args);
