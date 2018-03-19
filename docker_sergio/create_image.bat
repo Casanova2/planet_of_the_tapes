02c049b10000d7bc66877e0e3da5b9aa7d61693e
AsierRuano <asierruano@gmail.com>
2018-03-19 12:36:29 +0100

    Finished Docker and Spring development.

--- docker_sergio/create_image.bat
+++ docker_sergio/create_image.bat
@@ -1,2 +1,2 @@
-docker build https://github.com/Casanova2/planet_of_the_tapes.git#develop:docker_sergio -t pott/prueba1:develop
+docker build https://github.com/Casanova2/planet_of_the_tapes.git#develop:docker_sergio -t pott/pott
 pause
