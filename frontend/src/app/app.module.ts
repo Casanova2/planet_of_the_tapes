85bba9ec87d8e5e89e67ed62217847baf9cd8c23
CarlosLopGarcia <clopgarcia93@gmail.com>
2018-04-10 12:16:41 +0200

    Added publicComponent as our main view for unlogged users and logged users (not admin)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,11 +8,13 @@ import { LoginComponent } from './component/login/login.component';
 import { routing } from './app.routing';
 import { FormsModule } from '@angular/forms';
 import { HttpModule, JsonpModule } from '@angular/http';
+import { PublicComponent } from './component/public.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    PublicComponent
   ],
   imports: [BrowserModule, FormsModule, HttpModule, JsonpModule, routing],
   providers: [LoginService],
--- frontend/src/app/app.routing.ts
+++ frontend/src/app/app.routing.ts
@@ -2,9 +2,10 @@ import { Routes, RouterModule } from '@angular/router';
 
 import {LoginComponent} from '../app/component/login/login.component';
 import { AppComponent } from './app.component';
+import { PublicComponent } from './component/public.component';
 
 const appRoutes = [
-  { path: '', component: AppComponent, useAsDefault: true},
+  { path: '', component: PublicComponent, useAsDefault: true},
   { path: 'login', component: LoginComponent }
 ];
 
