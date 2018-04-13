5dacdb5cade2e4d1d291c204be545f7b822c2d84
CarlosLopGarcia <clopgarcia93@gmail.com>
2018-04-13 16:26:28 +0200

    - Minor changes LoginRestController. Hope it works!!!

--- backend/src/main/java/com/planetofthetapes/restController/LoginRestController.java
+++ backend/src/main/java/com/planetofthetapes/restController/LoginRestController.java
@@ -11,17 +11,21 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.planetofthetapes.entity.POrder;
 import com.planetofthetapes.entity.User;
+import com.fasterxml.jackson.annotation.JsonView;
 import com.planetofthetapes.component.*;
 
 @RestController
 public class LoginRestController{
 
+	public interface UserDetails extends User.Basic, User.OrderRelationUser, POrder.Basic {}
 	private static final Logger log = LoggerFactory.getLogger(LoginRestController.class);
 
 	@Autowired
 	private UserComponent userComponent;
 
+	@JsonView(UserDetails.class)
 	@RequestMapping("/api/logIn")
 	public ResponseEntity<User> logIn() {
 		userComponent.getLoggedUser();
