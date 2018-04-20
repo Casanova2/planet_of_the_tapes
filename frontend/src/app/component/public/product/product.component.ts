3c4d61fa09e0fdec21785b3afe98f47a51be7ca7
CarlosLopGarcia <clopgarcia93@gmail.com>
2018-04-20 11:17:44 +0200

    - Changes:  + Started and almost done the cart functionality. When you finish an order and start another one, logic goes to hell and create various orders. It's a for problem, but we can't control that tkae only the progress order instead of all of them.

--- frontend/src/app/component/public/product/product.component.ts
+++ frontend/src/app/component/public/product/product.component.ts
@@ -15,11 +15,12 @@ export class ProductComponent implements OnInit {
   img_url: string;
   private movieP :boolean;
   private orders: POrder [];
+  private order: POrder;
 
   constructor(private router: Router, private service: ProductService, private sanitizer: DomSanitizer, private activatedRoute: ActivatedRoute, 
     private oService: OrderService) {
-    this.img_url = PRODUCTS_IMG_URL;
-    this.movieP=false;
+    
+      this.img_url = PRODUCTS_IMG_URL;
   }
 
   ngOnInit() {
@@ -37,6 +38,8 @@ export class ProductComponent implements OnInit {
       },
       error => console.log(error)
     );
+
+
   }
 
   addToCart(){
@@ -48,13 +51,17 @@ export class ProductComponent implements OnInit {
       }
       
   updateCart(){
+
     for (let order of this.orders){
-      if (order.state = "progress"){
+    this.oService.getUOrder(order.id);
+      if (order.state == "progress"){
         this.oService.addProductToOrder(order, this.activatedRoute.snapshot.params['id']).subscribe(
           response => {  this.router.navigate(['/']);},
           error => console.log(error) 
         );
-        }
+      } else if (order.state == "completed"){
+        this.addToCart();
       }
     }
+}
 }
